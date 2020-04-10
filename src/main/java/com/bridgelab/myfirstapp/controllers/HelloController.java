ef38ca3fad9c519e2f9f5bd79b683300bc69ec5e
Shraddha2225 <shraddhat22m@gmail.com>
2020-04-10 11:47:32 +0530

    ADDED:PUT API To Update First And Last Name

--- src/main/java/com/bridgelab/myfirstapp/controllers/HelloController.java
+++ src/main/java/com/bridgelab/myfirstapp/controllers/HelloController.java
@@ -12,9 +12,20 @@ public class HelloController {
     public String paramHello(@RequestParam("FirstName") String firstName,@RequestParam("LastName") String lastName){
         return "Hello " + firstName + lastName;
    }
-   @PostMapping("/getparam/name")
+   @PostMapping("getparam/name")
     public String postBodyHello(@RequestBody User user){
-        return user.getFirstName() + "" +user.getLastName();
+        return user.getFirstName() + " " +user.getLastName();
+   }
+   @PutMapping("put/hello")
+   public String putBodyHello(@RequestBody User user){
+        User newUser = new User();
+        newUser.setFirstName("Shraddha");
+       newUser.setLastName("Meshram");
+
+       newUser.setFirstName(user.getFirstName());
+       newUser.setLastName(user.getLastName());
+
+       return user.getFirstName() + " " +user.getLastName();
    }
 
 }
--- src/main/java/com/bridgelab/myfirstapp/controllers/User.java
+++ src/main/java/com/bridgelab/myfirstapp/controllers/User.java
@@ -8,7 +8,16 @@ public class User {
     public String getFirstName() {
         return firstName;
     }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
